dd09efa4259fba196b0405a013f3a4ec950bd80f
francescogerardi-dev <francesco.gerardi@gmail.com>
2026-03-19 19:07:06 +0100

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyATggTs_IGw_KMu-y3I7Vre5HnKbeyhAm4",
+    authDomain: "myfitnesstracker-f2089.firebaseapp.com",
+    projectId: "myfitnesstracker-f2089",
+    storageBucket: "myfitnesstracker-f2089.firebasestorage.app",
+    messagingSenderId: "229779508819",
+    appId: "1:229779508819:web:d62f6a5e7abe89e75000a6"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const icons = { 
+    'Corsa': '🏃', 'Padel': '🎾', 'Stretching': '🧘', 
+    'Pesi': '🏋️', 'Routine': '📋', 'Riposo': '💤', 'Addominali': '🧱' 
+};
