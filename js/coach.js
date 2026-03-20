b7724786b2a2e07ba506f6ccf2714afd7bc1885d
francescogerardi-dev <francesco.gerardi@gmail.com>
2026-03-20 13:51:40 +0100

    Update coach.js

--- js/coach.js
+++ js/coach.js
@@ -63,7 +63,7 @@ export function updateSuggestion() {
     }else {
         const sortedByLast = Object.entries(lastSeen).sort((a, b) => a[1] - b[1]);
         const oldestSport = sortedByLast[0][0];
-        const diffDays = Math.floor((oggiMezzanotte - sortedByLast[0][1]) / (1000 * 60 * 60 * 24));
+        const diffDays = Math.floor((oggi  - sortedByLast[0][1]) / (1000 * 60 * 60 * 24));
         const missingTime = diffDays > 365 ? "molto tempo" : `${diffDays} giorni`;
         suggestion = `Coach: Riprendiamo con ${icons[oldestSport] || ''} ${oldestSport}? (Manca da ${missingTime})`;
     }
