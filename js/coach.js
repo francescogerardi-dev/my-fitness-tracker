629c1cf98d6043599d10199ca6cef86538448131
francescogerardi-dev <francesco.gerardi@gmail.com>
2026-03-20 14:00:06 +0100

    Update coach.js

--- js/coach.js
+++ js/coach.js
@@ -55,7 +55,9 @@ export function updateSuggestion() {
     if (activeStreaks.length > 0) {
         const topSports = activeStreaks.map(x => `${icons[x[0]] || ''} ${x[0]} (${x[1]} giorni 🔥)`);
         suggestion = `Serie attiva! Non mollare: ${topSports.join(' e ')}`;
-    } 
+    } else {
+        suggestion = "";
+    }
     /*} else {
         const sortedByLast = Object.entries(lastSeen).sort((a, b) => a[1] - b[1]);
         const oldestSport = sortedByLast[0][0];
