9ece92ee98b12aa0cbbb0792a2bf657af4ba3ef8
francescogerardi-dev <francesco.gerardi@gmail.com>
2026-03-20 09:04:38 +0100

    Create utils.js

---
+++ js/utils.js
@@ -0,0 +1,15 @@
+export function formatDateLocal(d) { return d.toISOString().split('T')[0]; }
+
+export function getMonday(d) { 
+    d = new Date(d); 
+    const day = d.getDay(); 
+    const diff = d.getDate() - day + (day === 0 ? -6 : 1); 
+    return new Date(d.setDate(diff)); 
+}
+
+export function getWeekNumber(d) {
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+    return Math.ceil((((d - yearStart) / 86400000) + 1)/7);
+}
