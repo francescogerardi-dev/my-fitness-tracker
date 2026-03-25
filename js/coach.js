ddaa5fd2bf5f80810a33dabdb9dc00073aefec3a
francescogerardi-dev <francesco.gerardi@gmail.com>
2026-03-25 13:21:19 +0100

    Update coach.js

--- js/coach.js
+++ js/coach.js
@@ -75,7 +75,7 @@ export function updateSuggestion() {
     if(window.fitnessDB.length > 0) {
         const last = [...window.fitnessDB].sort((a,b)=>new Date(b.date)-new Date(a.date)).find(r=>r.weight > 0);
     }
-    console.log("Data ultimo peso", last.date);
+    console.log("Data ultimo peso", last);
     
     // 3. IL FIX: Controlla se oggi hai fatto uno SPORT REALE
     // Ignora i record che sono solo "Riposo" o dove non c'è attività sportiva
