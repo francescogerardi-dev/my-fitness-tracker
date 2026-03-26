dea0b28e7a11f39de69c0a95a38022c7f2a65971
francescogerardi-dev <francesco.gerardi@gmail.com>
2026-03-26 10:19:48 +0100

    Update charts.js

--- js/charts.js
+++ js/charts.js
@@ -70,7 +70,7 @@ export function renderChart() {
                     const {x, y} = meta.data[i]; const acts = grouped[labels[i]]?.activities || [];
                     acts.forEach((a, idx) => { 
                         chart.ctx.font = '14px serif'; 
-                        console.log('test');
+                        console.log(icons[a]);
                         chart.ctx.fillText(icons[a] || '⚪', x-7, y - 20 - (idx*16)); 
                     });
                 });
