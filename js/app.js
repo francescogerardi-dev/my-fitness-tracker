11030aae65d9227ec1ef7ec52a96188c3118547f
francescogerardi-dev <francesco.gerardi@gmail.com>
2026-03-27 11:01:46 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ onAuthStateChanged(auth, (user) => {
         const q = query(collection(db, "users", user.uid, "records"), orderBy("date", "asc"));
         onSnapshot(q, (snapshot) => {
             window.fitnessDB = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-        
+            console.log(fitnessDB);
             // Esegui i rendering solo se le funzioni sono disponibili
             if (window.renderChart) window.renderChart();
             if (window.updateSuggestion) window.updateSuggestion();
