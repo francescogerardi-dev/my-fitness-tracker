bee34011e9dcdab98efe2ad629503f2783511b11
francescogerardi-dev <francesco.gerardi@gmail.com>
2026-03-27 10:49:37 +0100

    Update charts.js

--- js/charts.js
+++ js/charts.js
@@ -108,7 +108,7 @@ export function renderHeatmap() {
         days.forEach(d => {
             const acts = records.filter(r => r.date === d);
             // Logica Punteggio: 1 punto per sport, 0.5 per stretching
-            if(acts.some(r => !['Stretching', 'Riposo'].includes(r.type))) wScore += 1;
+            if(acts.some(r => !['Stretching', 'Riposo','Peso'].includes(r.type))) wScore += 1;
             else if(acts.some(r => r.type === 'Stretching')) wScore += 0.5;
         });
 
