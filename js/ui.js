b3f78d2daecc6acffe2e72a87e6a29686816a37a
francescogerardi-dev <francesco.gerardi@gmail.com>
2026-03-27 14:22:03 +0100

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -5,7 +5,7 @@ const icons = {
 };
 
 export function showTab(id) {
-    console.log('Passo per showTab con id');
+    console.log('ui.js showTab');
     // Rimuovi active da tutti i contenuti (usa .tab-content o .content a seconda del tuo HTML)
     document.querySelectorAll('.tab-content, .content').forEach(c => c.classList.remove('active'));
     document.querySelectorAll('.tab-btn').forEach(b => b.classList.remove('active'));
