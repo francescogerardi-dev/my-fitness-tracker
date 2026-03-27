97390763d2cac701c1a30dace1dec7106d020e1a
francescogerardi-dev <francesco.gerardi@gmail.com>
2026-03-27 12:23:44 +0100

    Update charts.js

--- js/charts.js
+++ js/charts.js
@@ -19,8 +19,8 @@ export function renderChart() {
     }
 
     document.getElementById('currentWeekLabel').innerText = `Sett. ${monday.getDate()}/${monday.getMonth()+1}`;
-    const counter1 = 0;
-    const counter2 = 0;
+    var counter1 = 0;
+    var counter2 = 0;
     console.log('Creo i dati da inserire nel chart');
     const grouped = window.fitnessDB.reduce((acc, curr) => {
         if(!acc[curr.date]) acc[curr.date] = { weight: null, activities: [] };
@@ -35,8 +35,8 @@ export function renderChart() {
         if(curr.weight) acc[curr.date].weight = curr.weight; return acc;
     }, {});
 
-    console.log('Riposo-->'||counter1);
-    console.log('Riposo-->'||counter2);
+    console.log(counter1);
+    console.log(counter2);
     // Calcolo Punteggio Costanza
     let score = 0;
     labels.forEach(day => {
