3b538bf694a1d51d67b17af2a1181ea11dfa2cd3
francescogerardi-dev <francesco.gerardi@gmail.com>
2026-04-09 15:29:14 +0200

    Update charts.js

--- js/charts.js
+++ js/charts.js
@@ -44,8 +44,8 @@ export function renderChart() {
     labels.forEach(day => {
         const totalacts = window.fitnessDB.filter(r => r.date === day);
         //if(totalacts.forEach(r => !['Stretching', 'Riposo','Peso'].includes(r.type))) fullscore += 1;
-        totalacts.forEach(r => {if(r.type !=='Stretching') fullscore += 1)});
-        else if(totalacts.forEach(r => r.type === 'Stretching')) fullscore += 0.5;
+        //totalacts.forEach(r => {if(r.type !=='Stretching') fullscore += 1)});
+        //else if(totalacts.forEach(r => r.type === 'Stretching')) fullscore += 0.5;
     });
     console.log('Fullscore');
     console.log(fullscore);
