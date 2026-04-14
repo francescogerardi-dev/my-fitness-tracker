2faee83a0774f6addd378156edf5d30f16e1d4b7
francescogerardi-dev <francesco.gerardi@gmail.com>
2026-04-14 14:56:14 +0200

    Update charts.js

--- js/charts.js
+++ js/charts.js
@@ -1,6 +1,6 @@
 // js/charts.js
-import { icons, db, auth,calcolaCostanza } from './config.js';
-import { getMonday, formatDateLocal, getWeekNumber,days2today,days2todayUp,isCurrentWeek } from './utils.js';
+import { icons, db, auth } from './config.js';
+import { getMonday, formatDateLocal, getWeekNumber,days2today,days2todayUp,isCurrentWeek,calcolaCostanza } from './utils.js';
 
 let mainChart = null;
 let analisiChart = null;
